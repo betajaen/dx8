46360cce4cc7fa32852be3c65b8866fe9ac344d1
Robin Southern <betajaen@ihoed.com>
2017-11-05 11:10:29 +0000

    Basic Files and Folder Setup

---
+++ Source/libDX8/clean_vs2017.bat
@@ -0,0 +1,8 @@
+del *.sln
+del *.vcxproj
+del *.vcxproj.user
+del *.vcxproj.filters
+
+rmdir /S /Q .vs
+rmdir /S /Q .obj
+rmdir /S /Q Build
---
+++ Source/libDX8/genie.lua
@@ -0,0 +1,36 @@
+solution "DX8"
+
+  configurations      { "Debug", "Release" }
+  platforms           { "x64" }
+
+  targetdir           "Build"
+  debugdir            "Build"
+
+  configuration "Debug"
+    defines           { "DEBUG",  }
+    flags             { "Symbols" }
+
+  configuration "Release"
+    defines           { "NDEBUG"}
+    flags             { "Optimize" }
+
+  project "libDX8"
+    kind            "SharedLib"
+    language        "C"
+    objdir          ".Obj"
+    flags           { "FatalWarnings" }
+    files           {
+      "*.c",
+      "*.h"
+  }
+
+  project "libDX8-DevKit"
+    kind            "SharedLib"
+    language        "C"
+    objdir          ".Obj"
+    flags           { "FatalWarnings" }
+    defines         ( "DX8_DEVKIT")
+    files           {
+      "dx8_DevKit.c",
+      "dx8.h"
+  }
